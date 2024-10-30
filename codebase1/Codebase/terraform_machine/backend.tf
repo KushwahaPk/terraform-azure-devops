80bc28eff0526add5ccd8a188db67c52fd40067d
Suraj <suraj@poonamkushwaha.onmicrosoft.com>
2024-10-30 14:14:50 +0000

    Updated backend.tf

--- codebase1/Codebase/terraform_machine/backend.tf
+++ codebase1/Codebase/terraform_machine/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "teststorage45678"
     container_name = "stfstatefile"
-    key = "terraform.tfstate"
+    key = "dev.terraform.tfstate"
     resource_group_name  = "devops-test-rg"
     # subscription_id = "00000000-0000-0000-0000-000000000000"
     # client_id = "00000000-0000-0000-0000-000000000000"

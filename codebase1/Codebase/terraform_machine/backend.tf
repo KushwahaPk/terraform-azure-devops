be597b0c0a0d60d8e9d90015de371a60b2563272
Poonam Ramdular Kushwaha <p.ramdular.kushwaha@accenture.com>
2024-10-30 11:52:01 +0000

    Updated backend.tf

--- codebase1/Codebase/terraform_machine/backend.tf
+++ codebase1/Codebase/terraform_machine/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "statebackupstg"
-    container_name = "tfstate"
+    storage_account_name = "teststorage45678"
+    container_name = "stfstatefile"
     key = "terraform.tfstate"
-    resource_group_name  = "statebackup-rg"
+    resource_group_name  = "devops-test-rg"
     # subscription_id = "00000000-0000-0000-0000-000000000000"
     # client_id = "00000000-0000-0000-0000-000000000000"
     # client_secret = "mysecret"
